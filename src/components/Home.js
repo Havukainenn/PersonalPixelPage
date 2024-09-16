d0c339d0aaa407eb2a570bc25a0135674e74504f
Roope <roopehavukainen@hotmail.com>
2024-09-16 23:41:28 +0300

    Fix, removing unused variable

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,9 @@
 // src/components/Home.js
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import './Home.css';  // Ensure you style it correctly
 import TypingIntroduction from './TypingIntroduction'; // Import the TypingIntroduction
 
 function Home() {
-  const navigate = useNavigate(); // Hook to navigate between routes
-
   return (
     <div className="home-container">
       <h1>WELCOME</h1>
